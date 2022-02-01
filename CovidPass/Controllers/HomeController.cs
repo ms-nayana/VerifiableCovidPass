1d918ec0a486f4495ec124cfe5aabb854d4b7ac3
nmannilthodi <nmannilthodi@LOANDEV2>
2022-02-02 00:27:44 +0100

    Add project files.

---
+++ CovidPass/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using CovidPass.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace CovidPass.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(UserInfo userInfo)
+        {
+            return RedirectToAction("Issue", userInfo);
+        }
+
+        public IActionResult Issue(UserInfo userInfo)
+        {
+            return View(userInfo);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ CovidPass/Models/UserInfo.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CovidPass.Models
+{
+    public class UserInfo
+    {
+        [Required]
+        [Display(Name = "First Name")]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Display(Name = "PhoneNumer")]
+        public string PhoneNumer { get; set; }
+    }
+}
